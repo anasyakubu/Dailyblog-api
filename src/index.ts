5d09cdc551204a463159b52e04caedda85da18d0
anasyakubu <yakubuanas04@gmail.com>
2025-06-02 12:30:32 +0100

    feat: add graphql-http dependency and update GraphQL endpoint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import mongoose from "mongoose";
-import postSchema from "./schema/post.schema";
+import postSchema from "./schema/post.schema.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,7 +15,10 @@ mongoose.connect(process.env.MONGO_URI || "your-mongo-uri", {})
   .catch(err => console.error(err));
 
 // GraphQL endpoint
-app.use("/graphql", graphqlHTTP({ postSchema, graphiql: true }));
+app.use("/graphql", graphqlHTTP({
+  schema: postSchema,  // Use 'schema' property
+  graphiql: true
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/graphql`);
